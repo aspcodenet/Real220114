84070562fa338a23ef489fdb6ad710b505c27a5a
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-14 09:31:55 +0100

    fixed la till flask db init också

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from flask_migrate import Migrate, upgrade
 
 
 app = Flask(__name__)
-app.config.from_object(__name__ + '.ConfigDebug')
+app.config.from_object('config')
+#app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+mysqlconnector://root:password@localhost/db1'    # File-based SQL database
 
-#db.app = app
+db.app = app
 db.init_app(app)
+migrate = Migrate(app,db)
+
+if __name__  == "__main__":
+    with app.app_context():
+        upgrade()
+    app.run()
 
 
