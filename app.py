43a04e82e7297a3a97b5e0dc5381331d3c5a13e7
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-14 09:14:02 +0100

    V3

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from models import db
+from flask_migrate import Migrate, upgrade
+
 
 app = Flask(__name__)
 app.config.from_object(__name__ + '.ConfigDebug')
+
+#db.app = app
+db.init_app(app)
 
 
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
+
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
